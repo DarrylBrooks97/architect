98f15ad3630dc462f7ada3a7daf8f0b90dff12ce
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-27 21:31:38 -0500

    feat: adding masonry

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
+import { useEffect } from 'react';
+import { NextImage } from '../components/NextImage';
 import { photos } from '../constants';
 import { RootObject } from '../utils';
 
 const Home = () => {
+  let msry: any;
+
+  useEffect(() => {
+    if (window) {
+      setTimeout(() => {
+        const Masonry = require('masonry-layout');
+        msry = new Masonry('.photos', {
+          columnWidth: 400,
+          stagger: 30,
+        });
+        console.log('finishsed!');
+        console.log({ msry });
+      }, 3000);
+    }
+    // () => msry.off();
+  }, []);
   return (
     <div className="w-screen h-screen flex justify-center items-center p-3">
-      <div className="flex-col text-center">
+      <div className="flex-col text-center w-full">
         {/* <p className="text-5xl font-bold animate-onload">Architect</p> */}
-        <div className="grid grid-cols-2 md:grid-cols-3">
+        <div className="photos">
           {new Array<RootObject>(5).fill(photos).map(p => (
-            <div className=""></div>
+            <div className="h-[200] w-full" key={p.id}>
+              <NextImage src={p.urls.small} alt={p.id} layout="fill" priority />
+            </div>
           ))}
         </div>
       </div>
