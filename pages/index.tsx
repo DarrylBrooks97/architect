d6bec1afab964d136b87001bab81f81eab59ad37
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-28 11:56:15 -0500

    feat: removing masonry

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,38 @@
-import { useEffect } from 'react';
 import { NextImage } from '../components/NextImage';
 import { photos } from '../constants';
 import { RootObject } from '../utils';
 
-const Home = () => {
-  let msry: any;
+interface PhotoProps {
+  photo: RootObject;
+  size: number | string;
+}
 
-  useEffect(() => {
-    if (window) {
-      setTimeout(() => {
-        const Masonry = require('masonry-layout');
-        msry = new Masonry('.photos', {
-          columnWidth: 400,
-          stagger: 30,
-        });
-        console.log('finishsed!');
-        console.log({ msry });
-      }, 3000);
-    }
-    // () => msry.off();
-  }, []);
+const Photo = (props: PhotoProps) => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center p-3">
-      <div className="flex-col text-center w-full">
+    <div
+      className={`my-4 mx-3 row-end-[span_${props.size.toString().slice(0, 1)}] relative h-[${
+        props.size
+      }]`}
+      key={props.photo.id}
+    >
+      <NextImage src={props.photo.urls.small} alt={props.photo.id} layout="fill" priority />
+    </div>
+  );
+};
+
+const Home = () => {
+  const randomSize = (): string => {
+    const rand = Math.floor(Math.random() * 3);
+    return rand === 0 ? '250px' : rand === 1 ? '350px' : '450px';
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center p-6">
+      <div className="flex-col text-center h-full w-full">
         {/* <p className="text-5xl font-bold animate-onload">Architect</p> */}
-        <div className="photos">
-          {new Array<RootObject>(5).fill(photos).map(p => (
-            <div className="h-[200] w-full" key={p.id}>
-              <NextImage src={p.urls.small} alt={p.id} layout="fill" priority />
-            </div>
+        <div className="grid absolute justify-center grid-cols-[repeat(auto-fill,250px)] left-1/2 -translate-x-2/4 h-full w-[80vw] auto-rows-[10px]">
+          {new Array<RootObject>(5).fill(photos).map(photo => (
+            <Photo photo={photo} size={randomSize()} key={photo.id} />
           ))}
         </div>
       </div>
