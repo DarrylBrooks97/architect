e9f13e54d43be80ffa8456e69346977b61945b7f
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-27 20:32:42 -0500

    chore: adding some animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        onload: 'onloadKeyFrame 3s ease-in-out forwards',
+        afterload: 'afterLoadKeyFrame 3s forwards',
+      },
+      keyframes: {
+        afterLoadKeyFrame: {
+          '0%': {
+            display: 'none',
+            opacity: '0',
+            animationDelay: '10s',
+            transform: 'translateY(0px)',
+          },
+          '50%': { display: 'block', transform: 'translateY(50px)' },
+          '100%': { opacity: '1', transform: 'translateY(100px)' },
+        },
+        onloadKeyFrame: {
+          '0%': { opacity: '0', display: 'none', transform: 'translateY(0px)' },
+          '50%': { opacity: '1', transform: 'translateY(-50px)' },
+          '100%': {
+            opacity: '0',
+            display: 'none',
+            transform: 'translateY(-100px)',
+            animationDelay: '2s',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
