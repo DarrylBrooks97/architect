d606b557b11180576b7ca6aa506d735daabd7900
Darryl Brooks <darrylbrooks13@gmail.com>
2023-03-30 01:31:43 -0500

    chore: linting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: {
-    extend: {
-      animation: {
-        onload: 'onloadKeyFrame 3s ease-in-out forwards',
-        afterload: 'afterLoadKeyFrame 3s forwards',
-      },
-      keyframes: {
-        afterLoadKeyFrame: {
-          '0%': {
-            display: 'none',
-            opacity: '0',
-            animationDelay: '10s',
-            transform: 'translateY(-100px)',
-          },
-          '100%': { display: 'block', opacity: '1', transform: 'translateY(0px)' },
-        },
-        onloadKeyFrame: {
-          '0%': { opacity: '0', display: 'none', transform: 'translateY(0px)' },
-          '50%': { opacity: '1', transform: 'translateY(-100px)' },
-          '100%': {
-            opacity: '0',
-            display: 'none',
-            transform: 'translateY(-200px)',
-            animationDelay: '2s',
-          },
-        },
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+	theme: {
+		extend: {
+			animation: {
+				onload: 'onloadKeyFrame 3s ease-in-out forwards',
+				afterload: 'afterLoadKeyFrame 3s forwards',
+			},
+			keyframes: {
+				afterLoadKeyFrame: {
+					'0%': {
+						display: 'none',
+						opacity: '0',
+						animationDelay: '10s',
+						transform: 'translateY(-100px)',
+						filter: 'blur(1px)',
+					},
+					'100%': { display: 'block', opacity: '1', transform: 'translateY(0px)' },
+				},
+				onloadKeyFrame: {
+					'0%': { opacity: '0', display: 'none', transform: 'translateY(0px)' },
+					'50%': { opacity: '1', transform: 'translateY(-100px)' },
+					'100%': {
+						opacity: '0',
+						display: 'none',
+						transform: 'translateY(-200px)',
+						animationDelay: '2s',
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
 };
